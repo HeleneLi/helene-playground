797c65afadff1743561018348fa78a595be53afd
Helene Lipp <ch.lipp@yahoo.com>
2022-01-29 15:12:24 +0000

    Initial Push

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='helene-playground',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/helene-playground-run'],
+      scripts=['scripts/helene-playground-run', 'scripts/helene-playground-computedist'],
       zip_safe=False)
